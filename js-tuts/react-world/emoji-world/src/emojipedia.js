5349728b7f43015c42428f6ea913991e54072b1a
alokkumax <alokkumax1@gmail.com>
2023-09-12 11:18:59 +0530

    added notes array

---
+++ js-tuts/react-world/emoji-world/src/emojipedia.js
@@ -0,0 +1,59 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "😈",
+    name: "Smiling Face With Horns",
+    meaning:
+      "Mischievously grinning goblin smiley with small devil horns. Wants to cause trouble, is a real teaser or planning something ugly just now."
+  },
+  {
+    id: 2,
+    emoji: "🐭",
+    name: "Mouse face",
+    meaning:
+      "Mouse face emoji 🐭 represent small and cute, but also shy and helpless animals. Often depicted in gray or white, they have large, round ears, a pink, pointed nose, and whiskers."
+  },
+  {
+    id: 3,
+    emoji: "🙄",
+    name: "Face With Rolling Eyes",
+    meaning:
+      "Smiley is rolling its eyes. Finds the current situation or a person boring or annoying. Will disregard the message."
+  },
+  {
+    id: 4,
+    emoji: "😡",
+    name: "Pouting Face",
+    meaning:
+      "The pouting face expresses displeasure with a person or situation. Beware, danger of explosion! Furious emoji face. Is annoyed and already sulking with a deep-red face."
+  },
+  {
+    id: 5,
+    emoji: "🥹 ",
+    name: "Face Holding Back Tears",
+    meaning:
+      "The 🥹 emoji, which shows a slightly smiling face with tears in its eyes, can express various emotions such as gratitude, emotion, or pride. At the same time, anger or rage can also be the reason why tears can hardly be held back."
+  },
+  {
+    id: 6,
+    emoji: "😘",
+    name: "Face Throwing a Kiss",
+    meaning:
+      "The kiss emoji 😘 is an expression of affection. It conveys friendly or romantic sympathy and is often used to express tenderness or as a flirtatious signal. The kissing emoji can also be used to express gratitude or to soften a sarcastic response."
+  },
+  {
+    id: 7,
+    emoji: "😋",
+    name: "Face Savouring Delicious Food",
+    meaning:
+      "Bon appetit! Licking your lips with pleasure, the 😋 emoji shows an expression of satisfaction, hunger or joy over delicious food. It is shown beaming with joy when you have cooked something particularly tasty or when your plate is empty."
+  },
+  {
+    id: 7,
+    emoji: "🧐",
+    name: "Face With Monocle",
+    meaning:
+      "With its monocle, the 🧐 emoji represents a face that was considered a status symbol in the 19th century. It shows furrowed eyebrows and a slightly raised head, as if examining something closely. "
+  }
+];
+export default emojipedia;
